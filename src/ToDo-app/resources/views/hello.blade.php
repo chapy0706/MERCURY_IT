340ede85c59e4f63ffc20160ddc23835971fc05d
chapy0706 <0xeca447abc7168eab67d279eafd33f39da9b54e40@ethermail.io>
2024-09-29 23:21:34 +0900

    #3 PHPの超基礎（Hello World）

---
+++ src/ToDo-app/resources/views/hello.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHPでHelo World</title>
+</head>
+<body>
+    <h1>PHPでHelo World</h1>
+    <?php $greeding = 'こんにちは、世界！'; ?>
+    <p><?php echo $greeding; ?></p>
+</body>
+</html>
