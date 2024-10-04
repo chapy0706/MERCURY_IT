f5987c233efb01e8a3b70fa3098b869d9e184a8c
chapy0706 <0xeca447abc7168eab67d279eafd33f39da9b54e40@ethermail.io>
2024-10-04 23:31:29 +0900

    #4 データ型

--- src/ToDo-app/resources/views/hello.blade.php
+++ src/ToDo-app/resources/views/hello.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>PHPでHelo World</title>
+    <title>PHPのデータ型</title>
 </head>
 <body>
     <?php $total = 500 * 1.1 ?>
@@ -10,5 +10,21 @@
     <p>税込金額は：<?php print $total; ?>円です</p>
     <p><?= '税込金額は：' . $total . '円です'  ?></p>
     <p><?php var_dump($total); ?></p>
+
+    <?php
+        # PHPの変数名は(lower)camelCaseを慣習的に用いるらしい
+        $integerValue = 10; // 整数型
+        $floatValue = 21.4; // 浮動小数点数型
+        $stringValue1 = '文字列1'; // 文字列型(シングルクオート)
+        $stringValue2 = "文字列2"; // 文字列型(ダブルクオート)
+        $boolValue = true; // 真偽値型
+        $nullValue = ''; // 空文字（文字列型）
+    ?>
+    <p>整数:<?php echo $integerValue; ?></p>
+    <p>浮動小数点数:<?php echo $floatValue; ?></p>
+    <p>文字列1:<?php echo $stringValue1; ?></p>
+    <p>文字列2:<?php echo $stringValue2; ?></p>
+    <p>真偽値:<?php echo $boolValue; ?></p>
+    <p>空文字:<?php echo $nullValue; ?></p>
 </body>
 </html>
