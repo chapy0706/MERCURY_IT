2f763d7431369d7c9177ac0de236bf28390c0a2b
chapy0706 <0xeca447abc7168eab67d279eafd33f39da9b54e40@ethermail.io>
2024-11-24 19:46:32 +0900

    #4 演算子(インクリメント・配列/連想配列の合成)

--- src/ToDo-app/resources/views/hello.blade.php
+++ src/ToDo-app/resources/views/hello.blade.php
@@ -2,23 +2,57 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>PHPのデータ型(定数)</title>
+    <title>PHPのデータ型(演算子)</title>
 </head>
 <body>
-    <?php 
-        define('TAX_PERSENT',0.1);
-        $total = 500 * (1 + TAX_PERSENT);
-        $numbers = [11, 22, 33, 44]; // 配列
-        $user = [
-            'name' => 'test master',
-            'age' => '15',
-            'place' => 'Okinawa',
-        ];
-        $user['hobby'] = 'walk';
-        $user['job'] = 'police';
-    ?>
+    <!-- 前置・後置インクリメント -->
+    <?php $num = 7; ?>
+    <?php $added = ++$num; ?>
+    
+    <p>前置インクリメント時のnum:<?=$num?></p>
+    <p>前置インクリメント時のadded:<?=$added?></p>
 
-    <p><pre><?php print_r($numbers)?></pre></p>
-    <p><pre><?php print_r($user)?></pre></p>
-    <p><?= '税込金額は：' . $total . '円です'  ?></p>
+    <?php $num = 7; ?>
+    <?php $added = $num++; ?>
+    
+    <p>後置インクリメント時のnum:<?=$num?></p>
+    <p>後置インクリメント時のadded:<?=$added?></p>
+
+    <!-- 配列の合成 -->
+    <?php
+        $arr1 = ['a','b'];
+        $arr2 = ['d','e','f'];
+        $mergedA = $arr1 + $arr2;
+        $mergedB = $arr2 + $arr1;
+        $mergedC = array_merge($arr1, $arr2);
+        $mergedD = array_merge($arr2, $arr1);
+    ?>
+    <p>配列の合成結果</p>
+    * merged A:<?php print_r($mergedA)?><br>
+    * merged B:<?php print_r($mergedB)?><br>
+    * merged C:<?php print_r($mergedC)?><br>
+    * merged D:<?php print_r($mergedD)?><br>
+
+    <!-- 連想配列の合成 -->
+    <?php
+        $arr3 = [
+            'a' => 'valueA',
+            'b' => 'valueB'
+        ];
+        $arr4 = [
+            'a' => 'valueC',
+            'b' => 'valueD',
+            'c' => 'valueE'
+        ];
+        $mergedE = $arr3 + $arr4;
+        $mergedF = $arr4 + $arr3;
+        $mergedG = array_merge($arr3, $arr4);
+        $mergedH = array_merge($arr4, $arr3);
+    ?>
+    <p>連想配列の合成結果</p>
+    * merged E:<?php print_r($mergedE)?><br>
+    * merged F:<?php print_r($mergedF)?><br>
+    * merged G:<?php print_r($mergedG)?><br>
+    * merged H:<?php print_r($mergedH)?><br>
+</body>
 </html>
