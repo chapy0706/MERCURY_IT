2e47f0893f1845d993261d9cbd43d51575ce94b3
chapy0706 <0xeca447abc7168eab67d279eafd33f39da9b54e40@ethermail.io>
2024-11-24 17:38:55 +0900

    #4 データ型(連想配列)

--- src/ToDo-app/resources/views/hello.blade.php
+++ src/ToDo-app/resources/views/hello.blade.php
@@ -2,33 +2,20 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>PHPのデータ型(ヒアドキュメント化)</title>
+    <title>PHPのデータ型(連想配列)</title>
 </head>
 <body>
     <?php 
-        $total = 500 * 1.1; 
-        $integerValue = 10; // 整数型
-        $floatValue = 21.4; // 浮動小数点数型
-        $stringValue1 = '文字列1'; // 文字列型(シングルクオート)
-        $stringValue2 = "文字列2"; // 文字列型(ダブルクオート)
-        $boolValue = true; // 真偽値型
-        $nullValue = ''; // 空文字（文字列型）
         $numbers = [11, 22, 33, 44]; // 配列
-        //Here Document
-        $testHereDocument = <<< DOC
-    <p>
-      税込金額は$total 円です
-      '税込金額は：' . $total . '円です'
+        $user = [
+            'name' => 'test master',
+            'age' => '15',
+            'place' => 'Okinawa',
+        ];
+        $user['hobby'] = 'walk';
+        $user['job'] = 'police';
+    ?>
 
-      整数:$integerValue
-      浮動小数点数:$floatValue
-      文字列1:$stringValue1
-      文字列2:$stringValue2
-      真偽値:$boolValue
-      空文字:$nullValue
-    </p>
-    DOC;?>
-    
-    <p><pre><?=$testHereDocument?></pre></p>
     <p><pre><?php print_r($numbers)?></pre></p>
+    <p><pre><?php print_r($user)?></pre></p>
 </html>
