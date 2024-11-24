5acde9d2da815e4c0b4be4ba15f69db82f626db2
chapy0706 <0xeca447abc7168eab67d279eafd33f39da9b54e40@ethermail.io>
2024-11-24 16:45:51 +0900

    #4 データ型(ヒアドキュメント化)

--- src/ToDo-app/resources/views/hello.blade.php
+++ src/ToDo-app/resources/views/hello.blade.php
@@ -2,29 +2,31 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>PHPのデータ型</title>
+    <title>PHPのデータ型(ヒアドキュメント化)</title>
 </head>
 <body>
-    <?php $total = 500 * 1.1 ?>
-    <p>税込金額は：<?php echo $total; ?>円です</p>
-    <p>税込金額は：<?php print $total; ?>円です</p>
-    <p><?= '税込金額は：' . $total . '円です'  ?></p>
-    <p><?php var_dump($total); ?></p>
-
-    <?php
-        # PHPの変数名は(lower)camelCaseを慣習的に用いるらしい
+    <?php 
+        $total = 500 * 1.1; 
         $integerValue = 10; // 整数型
         $floatValue = 21.4; // 浮動小数点数型
         $stringValue1 = '文字列1'; // 文字列型(シングルクオート)
         $stringValue2 = "文字列2"; // 文字列型(ダブルクオート)
         $boolValue = true; // 真偽値型
         $nullValue = ''; // 空文字（文字列型）
-    ?>
-    <p>整数:<?php echo $integerValue; ?></p>
-    <p>浮動小数点数:<?php echo $floatValue; ?></p>
-    <p>文字列1:<?php echo $stringValue1; ?></p>
-    <p>文字列2:<?php echo $stringValue2; ?></p>
-    <p>真偽値:<?php echo $boolValue; ?></p>
-    <p>空文字:<?php echo $nullValue; ?></p>
-</body>
+        //Here Document
+        $testHereDocument = <<< DOC
+    <p>
+      税込金額は$total 円です
+      '税込金額は：' . $total . '円です'
+
+      整数:$integerValue
+      浮動小数点数:$floatValue
+      文字列1:$stringValue1
+      文字列2:$stringValue2
+      真偽値:$boolValue
+      空文字:$nullValue
+    </p>
+    DOC;?>
+    
+    <p><pre><?=$testHereDocument?></pre></p>
 </html>
